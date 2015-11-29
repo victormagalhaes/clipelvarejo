b188542781aedae8876a3ffd04d00945d1222cf4
Daniel Daim <dandaim@github.com>
2015-11-29 13:18:55 -0200

    Retirando config errada do databases

--- clipelvarejo/settings.py
+++ clipelvarejo/settings.py
@@ -88,15 +88,7 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'NAME': 'clipelvarejo',
-        'USER': 'root',
-        'PASSWORD': '',
-    }
-}
+
 
 
 # Internationalization
