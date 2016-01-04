76c9b688e2f89b3d495b5312032a98cdf63532da
Daniel Daim <ddaim@horizonte.tv.br>
2016-01-04 13:09:36 -0200

    Trocando e-mail de destino e adicionando lógica de contato/orçamento

--- base/mail.py
+++ base/mail.py
@@ -6,32 +6,38 @@ from django.core.mail import EmailMessage
 class Email(object):
 
 	def __init__(self, data):
-
 		self.phone = data.get('phone')
 		self.message = data.get('message')
 		self.name = data.get('name')
 		self.email = data.get('email')
-		self.subject = 'Contato do site Clipel Varejo'
-		self.to = ['dandaim@gmail.com', ]
+		self.is_contact = True if data.get('type') == 'contato' else False
+		self.to = ['clipelvarejo@clipelvarejo.com.br', ]
 
 	def send_email(self):
 
 		try:
 			email = EmailMessage(
-				subject=self.subject, body=self._format_message(), from_email=settings.EMAIL_ADDRESS, to=self.to
+				subject=self._get_subject(), body=self._format_message(), from_email=settings.EMAIL_ADDRESS, to=self.to
 			)
 			email.content_subtype = 'html'
 			email.send(fail_silently=False)
 		except Exception, e:
 			raise e
 
+	def _get_subject(self):
+
+		if self.is_contact is True:
+			return "Contato - %s" % self.name
+		else:
+			return "Pedido/Orçamento - %s" % self.name
+
 	def _format_message(self):
 
-		contact = "<div>Contato: %s - %s</div>" % (self.name, self.phone)
+		contact = "<div>Solicitante: %s - %s</div>" % (self.name, self.phone)
 
 		mail = "<div>Email: %s</div>" % self.email
 
-		message = "<div>%s</div>" % self.message
+		message = "<div>Mensagem: %s</div>" % self.message
 
 		formatted_message = "<html>%s%s%s</html>" % (contact, mail, message)
 
--- base/static/js/contact_me.js
+++ base/static/js/contact_me.js
@@ -15,6 +15,7 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
+            var type = $("select#soflow").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
@@ -27,7 +28,8 @@ $(function() {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+                    type: type
                 },
                 cache: false,
                 success: function() {
